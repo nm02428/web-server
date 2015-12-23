50f489d5f117f4ae8c955c6e68866c650c803a73
Naresh Mongroo <nmongroo@hotmail.com>
2015-12-23 08:51:43 -0400

    new MW file

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next){
-		console.log ('private route hit');
-		next();
-	},
-	logger: function (req, res, next){
-		console.log('Request: ' + new Date().toString() + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
+
 app.use(middleware.logger);
+
 //app.use(middleware.requireAuthentication);
 //app.get('/', function (req, res){
 //	res.send('Hello express');
