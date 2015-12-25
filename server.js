56648956dfd260c33a57a9551e6cea83aec6ecc6
Naresh Mongroo <nmongroo@hotmail.com>
2015-12-24 20:34:07 -0400

    update port

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
-//app.use(middleware.requireAuthentication);
-//app.get('/', function (req, res){
-//	res.send('Hello express');
-//});
-
-app.get('/about',middleware.requireAuthentication, function (req, res){
+app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function(){
-	console.log('Express server started on port ' + PORT);
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
